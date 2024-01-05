a52f4c30daabc2440fc3357d1149db2d280d2462
hlalljie <hlalljie@gmail.com>
2024-01-05 11:53:29 -0800

    Found url from id

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 require('dotenv').config();
 const express = require('express');
 let bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 const cors = require('cors');
+var dns = require('dns');
 const app = express();
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+const urlSchema = new mongoose.Schema({
+  url: {
+    type: String,
+    required: true
+  }
+});
+
+let urlRecord = mongoose.model('Url', urlSchema);
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
@@ -22,12 +34,45 @@ app.get('/api/hello', function(req, res) {
 
 app.use("/api/shorturl", bodyParser.urlencoded({extended: false}));
 
+// Add url and show user the shortened url
 app.post("/api/shorturl",
-    function(req, res) {
-        let og_url = req.body.url;
-        res.send({original_url: og_url, short_url: 1});
+    (req, res) => {
+        const og_url = req.body.url;
+        try {
+          const urlObject = new URL(og_url);
+          dns.lookup(urlObject.hostname,  (err, addresses, family) => {
+            let newUrl = new urlRecord({url: og_url});
+            try {
+              newUrl.save();
+              res.json({original_url: og_url, short_url: newUrl._id});
+            }
+            catch(err){
+              console.error(err);
+              res.json({error:"Url cannot be created"});
+            }
+          });
+        }
+        catch(err){
+          res.json({error:"Invalid URL"})
+          return console.error(err);
+        }        
     }
 );
+
+app.get("/api/shorturl/:short_url",
+    async function(req, res) {
+      try{
+        const foundUrlRecord = await urlRecord.findById(req.params.short_url, 'url').lean();
+        foundUrl = foundUrlRecord.url;
+        res.json({url: foundUrl});
+      }
+      catch(err){
+        console.error(err);
+        res.send({error: "No URL found for that id"});
+      }
+    }
+);
+
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
