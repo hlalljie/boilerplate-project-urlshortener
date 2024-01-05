87ebdb48a98d07ec54ac189c01f70ecfaf87a6d6
hlalljie <hlalljie@gmail.com>
2024-01-04 22:52:15 -0800

    added post handler

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const express = require('express');
+let bodyParser = require('body-parser');
 const cors = require('cors');
 const app = express();
 
@@ -18,6 +19,15 @@ app.get('/', function(req, res) {
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
+
+app.use("/api/shorturl", bodyParser.urlencoded({extended: false}));
+
+app.post("/api/shorturl",
+    function(req, res) {
+        let og_url = req.body.url;
+        res.send({original_url: og_url, short_url: 1});
+    }
+);
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
